2f18169d550a422bdbb47b60188d53fedf74b6e8
Shivam Yadav <shivam10yadav05@gmail.com>
2026-08-01 19:15:14 +0530

    fix

--- frontend/src/localcomponents/ComponentsDetails.jsx
+++ frontend/src/localcomponents/ComponentsDetails.jsx
@@ -11,7 +11,7 @@ import {
   X,
 } from "lucide-react";
 import { components } from "../data/components";
-import { getHighlighter } from "../lib/highlighter";
+import { getHighlighter } from "../lib/Highlighter";
 
 const partNumber = (slug = "") => {
   let hash = 0;
